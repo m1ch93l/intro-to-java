4feee35fe906a6e40ae756a06fce57261c894ab4
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-04-12 15:05:33 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/intro/to/java/IntroToJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.intro.to.java;
+
+/**
+ *
+ * @author m1ch93l
+ */
+public class IntroToJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
