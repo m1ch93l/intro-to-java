9655b153703bbbd722c354eb2ccdaebd859259ec
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-04-14 23:32:35 +0800

    Update IntroToJava.java

--- src/main/java/com/mycompany/intro/to/java/IntroToJava.java
+++ src/main/java/com/mycompany/intro/to/java/IntroToJava.java
@@ -1,13 +1,13 @@
 package com.mycompany.intro.to.java;
 public class IntroToJava {
     public static void main(String[] args) {
-        String word = "Michael B. Banaria";
+        String text = "Michael B. Banaria";
         int number = 100;
         float decimal = 99.9f;
         char single_character = 'M';
         boolean true_or_false = true;
         
-        System.out.println(word);
+        System.out.println(text);
         System.out.println(number);
         System.out.println(decimal);
         System.out.println(single_character);
