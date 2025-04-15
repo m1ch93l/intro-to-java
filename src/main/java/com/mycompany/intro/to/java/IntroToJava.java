850f2a925bce31efda8b7c121adefa4fde38687d
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-04-16 00:10:03 +0800

    Add boolean sample

--- src/main/java/com/mycompany/intro/to/java/IntroToJava.java
+++ src/main/java/com/mycompany/intro/to/java/IntroToJava.java
@@ -11,5 +11,17 @@ public class IntroToJava {
         System.out.println(greeting +  ", " + txt);
         System.out.println(greeting.concat(txt));
         System.out.println("I love programming and \n to all tech subjects");
+        
+        boolean isJavaFun = true;
+        boolean isNotFun = false;
+        System.out.println(isJavaFun);
+        System.out.println(isNotFun);
+        
+        int number = 10;
+        System.out.println(number == 10);
+        
+        int myAge = 25;
+        int votingAge = 18;
+        System.out.println(myAge >= votingAge);
     }
 }
