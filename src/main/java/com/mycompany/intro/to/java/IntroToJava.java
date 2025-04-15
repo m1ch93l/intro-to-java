2a71d433863c3af131038c943208aa60afe39212
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-04-15 23:58:04 +0800

    Create string with concatenation

--- src/main/java/com/mycompany/intro/to/java/IntroToJava.java
+++ src/main/java/com/mycompany/intro/to/java/IntroToJava.java
@@ -1,16 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.intro.to.java;
-
-/**
- *
- * @author m1ch93l
- */
 public class IntroToJava {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String greeting = "Hello Good Day";
+        System.out.println(greeting);
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println(txt.length());
+        System.out.println(greeting.toUpperCase());
+        System.out.println(greeting.toLowerCase());
+        System.out.println(greeting.indexOf("G"));
+        System.out.println(greeting +  ", " + txt);
+        System.out.println(greeting.concat(txt));
+        System.out.println("I love programming and \n to all tech subjects");
     }
 }
