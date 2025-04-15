49765c41af5e32b0b15c76a092c240c28edc9e6c
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-04-16 00:47:10 +0800

    Create if else statements

--- src/main/java/com/mycompany/intro/to/java/IntroToJava.java
+++ src/main/java/com/mycompany/intro/to/java/IntroToJava.java
@@ -1,16 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.intro.to.java;
-
-/**
- *
- * @author m1ch93l
- */
 public class IntroToJava {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (20 > 18){System.out.println("Hello World!");}
+        
+        int number1 = 20, number2 = 18;
+        if (number1 > number2) {System.out.println("Hello World!");}
+        
+        if (number1 < number2) {System.out.println("Hello World!");}
+        else {System.out.println("Hello Programming");}
+        
+        int time = 22;
+        if (time < 10) {System.out.println("Good morning");}
+        else if (time < 18) {System.out.println("Good day");}
+        else {System.out.println("Good evening");}
+        
+        int timestamp = 20;
+        String result = (timestamp < 18) ? "Good day" : "Good evening";
+        System.out.println(result);
     }
 }
